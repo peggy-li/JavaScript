d214bf1fd65d2c2205844ad9b948de90f6a26d6e
peggy-li <peggy.li@duke.edu>
2013-02-06 01:57:37 -0500

    Added solution to q1-multiples

--- assignments/q1-multiples.js
+++ assignments/q1-multiples.js
@@ -1,3 +1,11 @@
+// Find the sum of all positive multiples of 3 or 5 below a specified number (limit).
+
 function multiples (limit) {
-  // ...
+	var sum = 0;
+	for (var i = 1; i < limit; i++) {
+		if (i % 3 == 0 || i % 5 == 0) {
+			sum = sum + i;
+		}
+	}
+	return sum;
 }
