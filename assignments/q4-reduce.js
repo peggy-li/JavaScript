a0d14b43f160baa631bf16572d45e33405b9b9f6
peggy-li <peggy.li@duke.edu>
2013-02-07 17:14:11 -0500

    Added solution to q4-reduce

--- assignments/q4-reduce.js
+++ assignments/q4-reduce.js
@@ -1,4 +1,10 @@
 function reduce (xs, fn, seed) {
-  // NOTE: that seed is optional; you should give the appropriate default in the function body
-  // ...
+ 	// NOTE: that seed is optional; you should give the appropriate default in the function body
+
+  	var init = (typeof seed === "undefined") ? xs[0] : seed;
+  	var i = (typeof seed === "undefined") ? 1 : 0;	
+
+  	for (i; i < xs.length; i++)
+  		init = fn(init, xs[i]);
+  	return init;
 }
